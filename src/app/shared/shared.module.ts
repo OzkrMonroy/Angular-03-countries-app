bce88399a4f43933497ffa7bdbd6fc7a3e3b3376
Oscar Monroy <oscarmonroy657@gmail.com>
2022-05-16 08:35:23 -0600

    Sidebar component is configured and ready.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 // Declarations has all of my components
@@ -13,7 +14,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
