e7928f3e32c5899f647fd4b2cde407edda1540bf
Oscar Monroy <oscarmonroy657@gmail.com>
2022-05-17 08:30:27 -0600

    Page to display details of a country is ready.

--- src/app/country/pages/see-country-details/see-country-details.component.ts
+++ src/app/country/pages/see-country-details/see-country-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-see-country-details',
@@ -7,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SeeCountryDetailsComponent implements OnInit {
+  country!: Country;
+  loading: boolean = true;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private countryService: CountryService) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.pipe(
+      switchMap(({ id }) => this.countryService.getCountries(id, 'alpha')),
+      tap(console.log)
+    ).subscribe({
+      next: (country) => {
+        this.country = country[0];
+        this.loading = false;
+      }
+    })
+
+    // this.activatedRoute.params.subscribe({
+    //   next: ({ id }) => {
+    //     this.countryService.getCountries(id, 'alpha').subscribe({
+    //       next: (country) => {
+    //         console.log(country);
+
+    //       }
+    //     })
+    //   }
+    // })
   }
 
 }
